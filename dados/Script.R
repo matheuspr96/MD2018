a87e9a46ab8868e41dd9f4cd36c0b85ef87e0264
matheuspr96 <matheuspr96@hotmail.com>
2018-08-23 12:54:11 -0300

    Arquivos script R e Dados internos do frame adicionado e alterado respectivamente

---
+++ dados/Script.R
@@ -0,0 +1,3 @@
+#!/bin/bash
+MinerioTeste <- read.csv("./dados/FCessoes_de_Direitos.csv")
+View(MinerioTeste)
---
+++ loadUFFS.R
@@ -0,0 +1,25 @@
+#script de remoção ultima linha
+uffs <- read.csv("FCessoes_de_Direitos.csv")
+
+uffs <- uffs[-nrow(uffs),]
+
+uffs <-tbl_df(uffs)
+
+uffs <- select(uffs,c("ano","nome"))
+uffs&desc_turma <- as.character(uffs&desc_turma)
+
+
+#executar script R source("endereco" "daods/script.R")
+#?? onde esta a funcao
+
+#list(1,2,"3") Suporta varios tipos de dados cotem um ponteiro e um vector de elementos [[1]]  [1] que tem 1
+#$ ACESSA os dados
+
+#enviroment env
+
+# vector a<-c(1,2,3,4)
+     #  b<-c(1,2,3,4)
+
+#rbind(a,b)
+
+#factor, ordinal-> Ordenado
