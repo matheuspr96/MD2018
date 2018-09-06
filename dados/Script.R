102dabed1c649c0d166f34f4ea9bf7dbd41d28b5
matheuspr96 <matheuspr96@hotmail.com>
2018-09-05 21:30:56 -0300

    Organizando pastas

--- dados/Script.R
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-FCessoes_de_Direitos <- read.csv("./dados/FCessoes_de_Direitos.csv")
-View(FCessoes_de_Direitos)
---
+++ dados/Script/Script.R
@@ -0,0 +1,3 @@
+#!/bin/bash
+FCessoes_de_Direitos <- read.csv("./dados/FCessoes_de_Direitos.csv")
+View(FCessoes_de_Direitos)
