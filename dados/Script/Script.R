dc429b753e22a2e5eb5e13cd347e3a66db4d5c6e
matheuspr96 <matheuspr96@hotmail.com>
2018-09-10 23:37:30 -0300

    Atividade 5

--- dados/Script/Script.R
+++ dados/Script/Script.R
@@ -1,3 +1,15 @@
 #!/bin/bash
-FCessoes_de_Direitos <- read.csv("./dados/FCessoes_de_Direitos.csv")
-View(FCessoes_de_Direitos)
+
+remove FCessoes_de_Direitos
+
+require(pacman)
+p_load(readr)
+
+FCessoes_de_Direitos <- read.csv2("./dados/CSVs/FCessoes_de_Direitos.csv")
+
+p_load(tidyverse)
+p_load(dplyr)
+
+attach(FCessoes_de_Direitos)
+ggplot(data=FCessoes_de_Direitos) + geom_bar(mapping = aes(x = Tipo.de.requerimento ,y = Superintendencia),stat = "identity")  + coord_flip()
+ggplot(data=FCessoes_de_Direitos, aes(x=FCessoes_de_Direitos$Superintendencia))+geom_bar() + coord_flip()
