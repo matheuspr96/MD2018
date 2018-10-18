62e2e60b3240cd9b42f745796fada7f222e0ab07
matheuspr96 <matheuspr96@hotmail.com>
2018-10-17 21:51:43 -0300

    Organização das atividades: Atividade_1.Rmd

--- dados/Script/Script.R
+++ dados/Script/Script.R
@@ -5,13 +5,10 @@ remove(FCessoes_de_Direitos)
 require(pacman)
 p_load(readr)
 
-FCessoes_de_Direitos <- read.csv2("./dados/CSVs/FCessoes_de_Direitos.csv")
+FCessoes_de_Direitos <- read.csv("./dados/CSVs/FCessoes_de_Direitos.csv")
 
 p_load(tidyverse)
 p_load(dplyr)
 
 attach(FCessoes_de_Direitos)
-ggplot(data=FCessoes_de_Direitos) + geom_bar(mapping = aes(x = Tipo.de.requerimento ,y = Superintendencia),stat = "identity")  + coord_flip()
-ggplot(data=FCessoes_de_Direitos, aes(x=FCessoes_de_Direitos$Superintendencia))+geom_bar() + coord_flip()
 
-install.packages("rmarkdown", contriburl = "http://cran.r-project.org/src/contrib", type = "source")
