82dfa108053b81ae7889418bd8550114813d51d0
matheuspr96 <matheuspr96@hotmail.com>
2018-10-18 08:21:11 -0300

    update atividade_4

---
+++ analise/Atividade_4/Script.R
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Verifica se possui um arquivo com o mesmo nome e remove
+remove(FCessoes_de_Direitos)
+
+#Depedencias
+require(pacman)
+
+p_load(readr)
+p_load(tidyverse)
+
+#Carrego o DataSet
+FCessoes_de_Direitos <- read.csv("./dados/CSVs/FCessoes_de_Direitos.csv")
+
+#Uso attach para facilitar na navegação dos elementos do DataSet
+attach(FCessoes_de_Direitos)
+
+
+
